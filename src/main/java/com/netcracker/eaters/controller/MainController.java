528ec7878e568e2e15f17fc1f5aa59ff3bfd2d32
Oleg Shaliakin <oleg.shaliakin@NetCracker.com>
2019-02-26 11:22:24 +0400

    feature (UI) add LogIn modal form

--- src/main/java/com/netcracker/eaters/controller/MainController.java
+++ src/main/java/com/netcracker/eaters/controller/MainController.java
@@ -2,8 +2,8 @@ package com.netcracker.eaters.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Controller
 public class MainController {
