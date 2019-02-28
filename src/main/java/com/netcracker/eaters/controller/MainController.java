f64e4fadc8ae96783c1e589fd2c0dd7e7350b501
vladjef <vladislav.efremenko@netcracker.com>
2019-02-28 10:11:07 +0400

    get/post methods for login form (draft)

--- src/main/java/com/netcracker/eaters/controller/MainController.java
+++ src/main/java/com/netcracker/eaters/controller/MainController.java
@@ -1,14 +1,32 @@
 package com.netcracker.eaters.controller;
 
+import com.netcracker.eaters.jdbc.Greeting;
+import com.netcracker.eaters.jdbc.SimpleExample;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 public class MainController {
-    @RequestMapping("/")
-    public String index(Model model) {
+
+    @GetMapping(value = {""} )
+    public String greetingForm(Model modelDB) {
+        modelDB.addAttribute("simpleExample", new SimpleExample());
+        return "index";
+    }
+
+    @PostMapping("/index")
+    public String greetingSubmit(@ModelAttribute SimpleExample greeting) {
         return "index";
     }
 }
--- src/main/resources/static/js/login_form.js
+++ src/main/resources/static/js/login_form.js
@@ -1,3 +1,7 @@
-document.getElementById("loginButton").onclick = function () {
-    location.href = "login_form.html";
+document.getElementById("singUp").onclick = function () {
+    //location.href = "login_form.html";
+    var txt1 = document.getElementById("firstname");
+    var txt2 = document.getElementById("lastname");
+
+    txt2.value = parseInt(txt2.value);
 }
