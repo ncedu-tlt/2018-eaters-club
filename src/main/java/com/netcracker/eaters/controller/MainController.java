179ef00273e4e195a01c814d6bf42eb23b6d46e9
Oleg.Shaliakin <Aligattor@mail.ru>
2019-03-11 02:22:21 +0400

    login page was added

--- src/main/java/com/netcracker/eaters/controller/MainController.java
+++ src/main/java/com/netcracker/eaters/controller/MainController.java
@@ -2,6 +2,7 @@ package com.netcracker.eaters.controller;
 
 import com.netcracker.eaters.jdbc.DBUsers;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,11 @@ public class MainController {
     @GetMapping("/")
     public String loadPage() {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "loginPage";
     }
 
     @PostMapping("/index")
